9162f80f94ed571da0efe260cb366a1338180a5f
iosdan-ferreira <iosdan.ferrreira@gmail.com>
2023-01-22 02:33:51 -0300

    firebase api key

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEX_PUBLIC_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.NEX_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEX_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEX_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEX_PUBLIC_FIREBASE_APP_ID,
-}
+    apiKey: "AIzaSyDRdFcwUPnw0xctIJ34gDoWzziDGKxi1Lc",
+    authDomain: "shopping-cart-7aff2.firebaseapp.com",
+    projectId: "shopping-cart-7aff2",
+    storageBucket: "shopping-cart-7aff2.appspot.com",
+    messagingSenderId: "208193646118",
+    appId: "1:208193646118:web:632cedf4f2b6fe89f940e7"
+  };
 
 initializeApp(firebaseConfig)
 
